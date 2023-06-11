aeedae16afd942ec5e95fd3c9b6a8d76b0a5eda7
MarcosDetalis <detalismarcos@gmail.com>
2023-06-11 15:46:30 -0400

    solucion al slider bar

--- src/App.jsx
+++ src/App.jsx
@@ -40,91 +40,99 @@ function App() {
                 <Frament> </Frament>
               </div>
             </nav>
-          </div>
-        </div>
-      </div>
-      <div id="content">
-        <div className="topbar">
-          <nav className="navbar navbar-expand-lg navbar-light">
-            <div className="full">
-              <button type="button" id="sidebarCollapse" class="sidebar_toggle">
-                <i class="fa fa-bars"></i>
-              </button>
-              <div class="logo_section">
-                <a href="index.html">
-                  <img
-                    class="img-responsive"
-                    src="images/logo/logo.png"
-                    alt="#"
-                  />
-                </a>
-              </div>
-              <div class="right_topbar">
-                <div class="icon_info">
-                  <ul>
-                    <li>
-                      <a href="#">
-                        <i class="fa fa-bell-o"></i>
-                        <span class="badge">2</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i class="fa fa-question-circle"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i class="fa fa-envelope-o"></i>
-                        <span class="badge">3</span>
-                      </a>
-                    </li>
-                  </ul>
-                  <ul class="user_profile_dd">
-                    <li>
-                      <a class="dropdown-toggle" data-toggle="dropdown">
+            <div id="content">
+              <div className="topbar">
+                <nav className="navbar navbar-expand-lg navbar-light">
+                  <div className="full">
+                    <button
+                      type="button"
+                      id="sidebarCollapse"
+                      className="sidebar_toggle"
+                    >
+                      <i className="fa fa-bars"></i>
+                    </button>
+                    <div className="logo_section">
+                      <a href="index.html">
                         <img
-                          class="img-responsive rounded-circle"
-                          src="images/layout_img/user_img.jpg"
+                          className="img-responsive"
+                          src="images/logo/logo.png"
                           alt="#"
                         />
-                        <span class="name_user">John David</span>
                       </a>
-                      <div class="dropdown-menu">
-                        <a class="dropdown-item" href="profile.html">
-                          My Profile
-                        </a>
-                        <a class="dropdown-item" href="settings.html">
-                          Settings
-                        </a>
-                        <a class="dropdown-item" href="help.html">
-                          Help
-                        </a>
-                        <a class="dropdown-item" href="#">
-                          <span>Log Out</span> <i class="fa fa-sign-out"></i>
-                        </a>
+                    </div>
+                    <div className="right_topbar">
+                      <div className="icon_info">
+                        <ul>
+                          <li>
+                            <a href="#">
+                              <i className="fa fa-bell-o"></i>
+                              <span className="badge">2</span>
+                            </a>
+                          </li>
+                          <li>
+                            <a href="#">
+                              <i className="fa fa-question-circle"></i>
+                            </a>
+                          </li>
+                          <li>
+                            <a href="#">
+                              <i className="fa fa-envelope-o"></i>
+                              <span className="badge">3</span>
+                            </a>
+                          </li>
+                        </ul>
+                        <ul className="user_profile_dd">
+                          <li>
+                            <a
+                              className="dropdown-toggle"
+                              data-toggle="dropdown"
+                            >
+                              <img
+                                className="img-responsive rounded-circle"
+                                src="images/layout_img/user_img.jpg"
+                                alt="#"
+                              />
+                              <span className="name_user">John David</span>
+                            </a>
+                            <div className="dropdown-menu">
+                              <a className="dropdown-item" href="profile.html">
+                                My Profile
+                              </a>
+                              <a className="dropdown-item" href="settings.html">
+                                Settings
+                              </a>
+                              <a className="dropdown-item" href="help.html">
+                                Help
+                              </a>
+                              <a className="dropdown-item" href="#">
+                                <span>Log Out</span>{" "}
+                                <i className="fa fa-sign-out"></i>
+                              </a>
+                            </div>
+                          </li>
+                        </ul>
                       </div>
-                    </li>
-                  </ul>
+                    </div>
+                  </div>
+                </nav>
+              </div>
+
+              <div className="midde_cont">
+                <div className="container-fluid">
+                  <Routes>
+                    <Route path="/Login" element={<Login />} />
+                    <Route path="/Registro" element={<Registro />} />
+                    <Route path="/Ver_infrom" element={<Ver_infromes />} />
+                    <Route path="/hook" element={<Hook />} />
+                    <Route path="/Registrar_user" element={<AgregarUser />} />
+                    <Route path="/Eliminar" element={<EliminarLibros />} />
+                    <Route path="/Solicitud" element={<Solicitud />}></Route>
+
+                    <Route path="/Solicitud2" element={<Solicitud2 />}></Route>
+                  </Routes>
                 </div>
               </div>
             </div>
-          </nav>
-        </div>
-
-        <div class="midde_cont">
-          <div class="container-fluid">
-            <Routes>
-              <Route path="/Login" element={<Login />} />
-              <Route path="/Registro" element={<Registro />} />
-              <Route path="/Ver_infrom" element={<Ver_infromes />} />
-              <Route path="/hook" element={<Hook />} />
-              <Route path="/Registrar_user" element={<AgregarUser />} />
-              <Route path="/Eliminar" element={<EliminarLibros />} />
-              <Route path="/Solicitud" element={<Solicitud />}></Route>
-
-              <Route path="/Solicitud2" element={<Solicitud2 />}></Route>
-            </Routes>
           </div>
         </div>
       </div>
