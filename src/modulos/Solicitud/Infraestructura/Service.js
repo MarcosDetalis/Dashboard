4de2bb83cd824ac133f1a3aa1fcca384160045a5
pablomaida <pablomaidana110@gmail.com>
2023-07-13 20:47:46 -0400

    commit rama

--- src/modulos/Solicitud/Infraestructura/Service.js
+++ src/modulos/Solicitud/Infraestructura/Service.js
@@ -1,28 +1,100 @@
 export async function getdatos() {
-    const url = "http://localhost:4005/ping";
-    const respuesta = await fetch(url);
-    const datos = await respuesta.json();
-    return datos;
-  }
+  const url = "https://servicedeb.onrender.com/api/lisreserva";
+  const respuesta = await fetch(url);
+  const datos = await respuesta.json();
+  return datos;
+}
 
-// no necesariamente tiene q llarse como en la DB. ejemplo:id
-  export async function DeleteReserva(id) {
-    try {
-      let res = await fetch("http://localhost:4005/api/eliminar", {
-        headers: { "Content-Type": "application/json" },
-        method: "DELETE",
-        body: JSON.stringify({
-          id: id,
-        }),
-      });
-      await res.json();
-      if (res.status === 201) {
-        console.log("Eliminado con exito");
-        location.reload();//refresca la pagina pa borrar de la taal el registro es temporal
-      } else {
-        console.log("error al al eliminar ");
-      }
-    } catch (err) {
-      console.log(err);
+// no necesariamente tiene q llamarrse como en la DB. ejemplo:id
+export async function DeleteReserva(id, nombre) {
+  try {
+    let res = await fetch("https://servicedeb.onrender.com/api/eliminar", {
+      headers: { "Content-Type": "application/json" },
+      method: "DELETE",
+      body: JSON.stringify({
+        id: id,
+        nombre: nombre,
+      }),
+    });
+    await res.json();
+    if (res.status === 201) {
+      console.log("Eliminado con exito");
+      location.reload(); //refresca la pagina pa borrar de la taal el registro es temporal
+    } else {
+      console.log("error al al eliminar ");
     }
+  } catch (err) {
+    console.log(err);
   }
+}
+
+// servicio pa actualizar estado de las reservas
+export async function updateReservas(id, estado, estadoo) {
+  try {
+    let res = await fetch("https://servicedeb.onrender.com/api/update", {
+      headers: { "Content-Type": "application/json" },
+      method: "PUT", //por que afecto solo 3 campos
+      body: JSON.stringify({
+        id: id,
+        estado: estado,
+        estadoo: estadoo,
+      }),
+    });
+    await res.json();
+    if (res.status === 201) {
+      console.log("Enviadoo con exito");
+      location.reload();
+    } else {
+      console.log("error al enviar ");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// servicio pa actualizar estado anulado
+export async function anularReservas(id, estado, estadoo) {
+  try {
+    let res = await fetch("https://servicedeb.onrender.com/api/anular", {
+      headers: { "Content-Type": "application/json" },
+      method: "PUT", //por que afecto solo 3 campos
+      body: JSON.stringify({
+        id: id,
+        estado: estado,
+        estadoo: estadoo,
+      }),
+    });
+    await res.json();
+    if (res.status === 201) {
+      console.log("Enviadoo con exito");
+      location.reload();
+    } else {
+      console.log("error al enviar ");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function pendienteReservas(id, estado, estadoo) {
+  try {
+    let res = await fetch("https://servicedeb.onrender.com/api/pendiente", {
+      headers: { "Content-Type": "application/json" },
+      method: "PUT", //por que afecto solo 3 campos
+      body: JSON.stringify({
+        id: id,
+        estado: estado,
+        estadoo: estadoo,
+      }),
+    });
+    await res.json();
+    if (res.status === 201) {
+      console.log("Enviadoo con exito");
+      location.reload();
+    } else {
+      console.log("error al enviar ");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
