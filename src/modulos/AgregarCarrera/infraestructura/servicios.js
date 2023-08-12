977a6b021c5c91ed7600ad7953bcfb57c866235e
pablomaida <pablomaidana110@gmail.com>
2023-08-12 19:53:37 -0400

    ss

--- src/modulos/AgregarCarrera/infraestructura/servicios.js
+++ src/modulos/AgregarCarrera/infraestructura/servicios.js
@@ -3,7 +3,40 @@ import withReactContent from "sweetalert2-react-content";
 
 const MySwal = withReactContent(Swal);
 
+export async function Agregar_carrera(inputcarrera) {
+ try {
+   let res = await fetch("http://localhost:4005/car/agregacarrera", {  //https://apiser.onrender.com
+     headers: { "Content-Type": "application/json" },
+     method: "POST",
+     body: JSON.stringify({
+      car_nombre:inputcarrera
+      
+     }),
+   });
+   await res.json();
+   if (res.status === 201) {
+     console.log("Enviadoo con exito");
+     Swal.fire({
+       position: "top-end",
+       icon: "success",
+       title: "Datos Guardados",
+       showConfirmButton: false,
+       timer: 1500,
+     });
+     location.reload();
+   } else {
+     console.log("error al AgregAR ");
 
+     MySwal.fire({
+       icon: "error",
+       title: "Ocurrio un error",
+       text: "Favor verificar los datos introducidos",
+     });
+   }
+ } catch (err) {
+   console.log(err);
+ }
+}
 
 
 
@@ -69,9 +102,9 @@ export async function eliminarusu(idCarrera) {
 
 ///---
 
-export async function PutReservas(nombrecarrera,idcarrare) {
+export async function PutReservas(idcarrare,nombrecarrera) {
   try {
-    let res = await fetch("http://localhost:4005/api/update", {
+    let res = await fetch("http://localhost:4005/car/updatecarrera", {
       headers: { "Content-Type": "application/json" },
       method: "PUT",
       body: JSON.stringify({
